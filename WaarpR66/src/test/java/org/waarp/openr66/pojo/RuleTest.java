c76e04173d8d16cee9350d2fd96ed289bf62ed7f
Bruno Carlin <bruno.carlin@waarp.org>
2020-02-09 16:33:32 +0100

    restore 3.0.x responses for Rule

---
+++ WaarpR66/src/test/java/org/waarp/openr66/pojo/RuleTest.java
@@ -0,0 +1,43 @@
+package org.waarp.openr66.pojo;
+
+import org.junit.Test;
+
+import org.waarp.common.json.JsonHandler;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RuleTest {
+
+	@Test
+	public void testJson() {
+        List<String> hosts = Arrays.asList("sup1");
+        List<RuleTask> tasks = Arrays.asList();
+		Rule tested = new Rule("rulename", 3, hosts, "recv-path", "send-path",
+            "arch-path", "work-path", tasks, tasks, tasks, tasks, tasks, tasks,
+            UpdatedInfo.TOSUBMIT);
+
+        String expected = "{" +
+            "\"IDRULE\":\"rulename\"," +
+            "\"MODETRANS\":3," +
+            "\"RECVPATH\":\"recv-path\"," +
+            "\"SENDPATH\":\"send-path\"," +
+            "\"ARCHIVEPATH\":\"arch-path\"," +
+            "\"WORKPATH\":\"work-path\"," +
+            "\"UPDATEDINFO\":3," +
+            "\"HOSTIDS\":\"<hostids><hostid>sup1</hostid></hostids>\"," +
+            "\"RPRETASKS\":\"<tasks></tasks>\"," +
+            "\"RPOSTTASKS\":\"<tasks></tasks>\"," +
+            "\"SPRETASKS\":\"<tasks></tasks>\"," +
+            "\"SPOSTTASKS\":\"<tasks></tasks>\"," +
+            "\"RERRORTASKS\":\"<tasks></tasks>\"," +
+            "\"SERRORTASKS\":\"<tasks></tasks>\"" +
+            "}";
+
+		String got = JsonHandler.writeAsString(tested);
+
+		assertEquals(expected, got);
+	}
+}
