5a743dbd544047f1ee266c1d7989b4934c482e4c
Bruno Carlin <bruno.carlin@waarp.org>
2020-02-14 14:24:28 +0100

    Fixes failing tests on Transfer

--- WaarpR66/src/test/java/org/waarp/openr66/pojo/RuleTest.java
+++ WaarpR66/src/test/java/org/waarp/openr66/pojo/RuleTest.java
@@ -1,28 +1,43 @@
 package org.waarp.openr66.pojo;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import org.waarp.common.json.JsonHandler;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class RuleTest {
 
 	@Test
-    @Ignore
 	public void testJson() {
         List<String> hosts = Arrays.asList("sup1");
-        List<RuleTask> tasks = Arrays.asList();
+
+        List<RuleTask> spretasks = new ArrayList<RuleTask>();
+        spretasks.add(new RuleTask("LOG", "spretasks", 0));
+
+        List<RuleTask> sposttasks = new ArrayList<RuleTask>();
+        sposttasks.add(new RuleTask("LOG", "sposttasks", 0));
+
+        List<RuleTask> serrortasks = new ArrayList<RuleTask>();
+        serrortasks.add(new RuleTask("LOG", "serrortasks", 0));
+
+        List<RuleTask> rpretasks = new ArrayList<RuleTask>();
+        rpretasks.add(new RuleTask("LOG", "rpretasks", 0));
+
+        List<RuleTask> rposttasks = new ArrayList<RuleTask>();
+        rposttasks.add(new RuleTask("LOG", "rposttasks", 0));
+
+        List<RuleTask> rerrortasks = new ArrayList<RuleTask>();
+        rerrortasks.add(new RuleTask("LOG", "rerrortasks", 0));
+
 		Rule tested = new Rule("rulename", 3, hosts, "recv-path", "send-path",
-            "arch-path", "work-path", tasks, tasks, tasks, tasks, tasks, tasks,
-            UpdatedInfo.TOSUBMIT);
+            "arch-path", "work-path", rpretasks, rposttasks, rerrortasks,
+            spretasks, sposttasks, serrortasks, UpdatedInfo.TOSUBMIT);
 
         String expected = "{" +
             "\"IDRULE\":\"rulename\"," +
@@ -33,12 +48,12 @@ public class RuleTest {
             "\"WORKPATH\":\"work-path\"," +
             "\"UPDATEDINFO\":3," +
             "\"HOSTIDS\":\"<hostids><hostid>sup1</hostid></hostids>\"," +
-            "\"RPRETASKS\":\"<tasks></tasks>\"," +
-            "\"RPOSTTASKS\":\"<tasks></tasks>\"," +
-            "\"SPRETASKS\":\"<tasks></tasks>\"," +
-            "\"SPOSTTASKS\":\"<tasks></tasks>\"," +
-            "\"RERRORTASKS\":\"<tasks></tasks>\"," +
-            "\"SERRORTASKS\":\"<tasks></tasks>\"" +
+            "\"SPRETASKS\":\"<tasks><task><type>LOG</type><path>spretasks</path><delay>0</delay></task></tasks>\"," +
+            "\"SERRORTASKS\":\"<tasks><task><type>LOG</type><path>serrortasks</path><delay>0</delay></task></tasks>\"," +
+            "\"RPRETASKS\":\"<tasks><task><type>LOG</type><path>rpretasks</path><delay>0</delay></task></tasks>\"," +
+            "\"RPOSTTASKS\":\"<tasks><task><type>LOG</type><path>rposttasks</path><delay>0</delay></task></tasks>\"," +
+            "\"RERRORTASKS\":\"<tasks><task><type>LOG</type><path>rerrortasks</path><delay>0</delay></task></tasks>\"," +
+            "\"SPOSTTASKS\":\"<tasks><task><type>LOG</type><path>sposttasks</path><delay>0</delay></task></tasks>\"" +
             "}";
 
 		String got = JsonHandler.writeAsString(tested);
