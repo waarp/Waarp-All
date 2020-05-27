90ddc15bfa10d8eb699ce1c77ce8be386b046b75
Bruno Carlin <bruno.carlin@waarp.org>
2020-05-27 19:17:12 +0200

    Fix the API browser for REST v2
    It was not working with modern browsers because it used deprecated javascript APIs.

--- doc/waarp-r66/source/_static/restv2/html/es5-bundle/bower_components/codemirror/mode/diff/diff.js
+++
@@ -1,6 +0,0 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: https://codemirror.net/LICENSE
-(function(mod){if("object"==("undefined"===typeof exports?"undefined":babelHelpers.typeof(exports))&&"object"==("undefined"===typeof module?"undefined":babelHelpers.typeof(module)))// CommonJS
-mod(require("../../lib/codemirror"));else if("function"==typeof define&&define.amd)// AMD
-define(["../../lib/codemirror"],mod);else// Plain browser env
-mod(CodeMirror)})(function(CodeMirror){"use strict";CodeMirror.defineMode("diff",function(){var TOKEN_NAMES={"+":"positive","-":"negative","@":"meta"};return{token:function token(stream){var tw_pos=stream.string.search(/[\t ]+?$/);if(!stream.sol()||0===tw_pos){stream.skipToEnd();return("error "+(TOKEN_NAMES[stream.string.charAt(0)]||"")).replace(/ $/,"")}var token_name=TOKEN_NAMES[stream.peek()]||stream.skipToEnd();if(-1===tw_pos){stream.skipToEnd()}else{stream.pos=tw_pos}return token_name}}});CodeMirror.defineMIME("text/x-diff","diff")});
--- doc/waarp-r66/source/_static/restv2/html/es6-bundle/bower_components/prism/components/prism-wiki.js
+++
@@ -1,9 +0,0 @@
-Prism.languages.wiki=Prism.languages.extend("markup",{"block-comment":{pattern:/(^|[^\\])\/\*[\s\S]*?\*\//,lookbehind:!0,alias:"comment"},heading:{pattern:/^(=+).+?\1/m,inside:{punctuation:/^=+|=+$/,important:/.+/}},emphasis:{// TODO Multi-line
-pattern:/('{2,5}).+?\1/,inside:{"bold italic":{pattern:/(''''').+?(?=\1)/,lookbehind:!0},bold:{pattern:/(''')[^'](?:.*?[^'])?(?=\1)/,lookbehind:!0},italic:{pattern:/('')[^'](?:.*?[^'])?(?=\1)/,lookbehind:!0},punctuation:/^''+|''+$/}},hr:{pattern:/^-{4,}/m,alias:"punctuation"},url:[/ISBN +(?:97[89][ -]?)?(?:\d[ -]?){9}[\dx]\b|(?:RFC|PMID) +\d+/i,/\[\[.+?\]\]|\[.+?\]/],variable:[/__[A-Z]+__/,// FIXME Nested structures should be handled
-// {{formatnum:{{#expr:{{{3}}}}}}}
-/\{{3}.+?\}{3}/,/\{\{.+?\}\}/],symbol:[/^#redirect/im,/~{3,5}/],// Handle table attrs:
-// {|
-// ! style="text-align:left;"| Item
-// |}
-"table-tag":{pattern:/((?:^|[|!])[|!])[^|\r\n]+\|(?!\|)/m,lookbehind:!0,inside:{"table-bar":{pattern:/\|$/,alias:"punctuation"},rest:Prism.languages.markup.tag.inside}},punctuation:/^(?:\{\||\|\}|\|-|[*#:;!|])|\|\||!!/m});Prism.languages.insertBefore("wiki","tag",{// Prevent highlighting inside <nowiki>, <source> and <pre> tags
-nowiki:{pattern:/<(nowiki|pre|source)\b[\s\S]*?>[\s\S]*?<\/\1>/i,inside:{tag:{pattern:/<(?:nowiki|pre|source)\b[\s\S]*?>|<\/(?:nowiki|pre|source)>/i,inside:Prism.languages.markup.tag.inside}}}});
