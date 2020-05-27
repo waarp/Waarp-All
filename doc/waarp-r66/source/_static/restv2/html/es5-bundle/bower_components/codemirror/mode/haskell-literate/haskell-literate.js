938c5f7f1c14314d1c18d081fbd024e8d4779f08
Bruno Carlin <bruno.carlin@waarp.org>
2020-05-27 19:17:12 +0200

    Fix the API browser for REST v2
    It was not working with modern browsers because it used deprecated javascript APIs.

--- doc/waarp-r66/source/_static/restv2/html/es5-bundle/bower_components/codemirror/mode/haskell-literate/haskell-literate.js
+++
@@ -1,6 +0,0 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: https://codemirror.net/LICENSE
-(function(mod){if("object"==("undefined"===typeof exports?"undefined":babelHelpers.typeof(exports))&&"object"==("undefined"===typeof module?"undefined":babelHelpers.typeof(module)))// CommonJS
-mod(require("../../lib/codemirror"),require("../haskell/haskell"));else if("function"==typeof define&&define.amd)// AMD
-define(["../../lib/codemirror","../haskell/haskell"],mod);else// Plain browser env
-mod(CodeMirror)})(function(CodeMirror){"use strict";CodeMirror.defineMode("haskell-literate",function(config,parserConfig){var baseMode=CodeMirror.getMode(config,parserConfig&&parserConfig.base||"haskell");return{startState:function startState(){return{inCode:/* eat */ /* ignoreName */ /* ignoreName */!1/* skipSlots */ /* skipSlots */,baseState:CodeMirror.startState(baseMode)}},token:function token(stream,state){if(stream.sol()){if(state.inCode=stream.eat(">"))return"meta"}if(state.inCode){return baseMode.token(stream,state.baseState)}else{stream.skipToEnd();return"comment"}},innerMode:function innerMode(state){return state.inCode?{state:state.baseState,mode:baseMode}:null}}},"haskell");CodeMirror.defineMIME("text/x-literate-haskell","haskell-literate")});
---
+++ doc/waarp-r66/source/_static/restv2/html/sw.js
@@ -0,0 +1,71 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "apic-import-6db60405.js",
+    "revision": "4e5c28a862f37f8a215740ad93dbee70"
+  },
+  {
+    "url": "index.html",
+    "revision": "68564b439525051ab2dceae0b366ecf7"
+  },
+  {
+    "url": "legacy/apic-import-5fcdaa97.js",
+    "revision": "5813f5c260c35c1b7ffc7afbc12f41d8"
+  },
+  {
+    "url": "polyfills/core-js.da8d8819d4fcddc05b82706be190ca82.js",
+    "revision": "280dc6d955e57fd263fd0f3f9741c059"
+  },
+  {
+    "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",
+    "revision": "cff507bc95ad1d6bf1a415cc9c8852b0"
+  },
+  {
+    "url": "polyfills/dynamic-import.991be47e17117abb5eb15f5254ad3869.js",
+    "revision": "08b86a7f56c6f0d65265654299b16d74"
+  },
+  {
+    "url": "polyfills/fetch.191258a74d74243758f52065f3d0962a.js",
+    "revision": "fcdc4efda1fe1b52f814e36273ff745d"
+  },
+  {
+    "url": "polyfills/regenerator-runtime.9090ed1c23690e3072c21a7873cad285.js",
+    "revision": "9af9d9e480dfccc420d30729e319b821"
+  },
+  {
+    "url": "polyfills/systemjs.ea78b72d7ee1221677a4143e4446db24.js",
+    "revision": "4de224f987266803124fca635fa63db5"
+  },
+  {
+    "url": "polyfills/webcomponents.6954abecfe8b165751e6bc9b0af6c639.js",
+    "revision": "894a294495257c3d389efa3e1bd9bde7"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
